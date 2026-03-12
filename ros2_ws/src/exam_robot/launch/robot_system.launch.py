08c00155c06868ac3aa77a6960dc35a9de890dc9
saybe2 <saybe2@users.noreply.github.com>
2026-03-12 17:51:58 +0300

    Add joint_state_publisher for TF and joint states

--- ros2_ws/src/exam_robot/launch/robot_system.launch.py
+++ ros2_ws/src/exam_robot/launch/robot_system.launch.py
@@ -43,5 +43,15 @@ def generate_launch_description():
                 output="screen",
                 parameters=[{"robot_description": robot_description}],
             ),
+            Node(
+                package="joint_state_publisher",
+                executable="joint_state_publisher",
+                name="joint_state_publisher",
+                output="screen",
+                parameters=[
+                    {"robot_description": robot_description},
+                    {"rate": 30.0},
+                ],
+            ),
         ]
     )
